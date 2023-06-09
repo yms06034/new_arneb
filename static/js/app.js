7545af4e5d3f1d63a032b2a9dfd1228a1497fd58
yms06034 <yms06034@gamil.com>
2023-06-09 14:05:57 +0900

    ch

--- static/js/app.js
+++ static/js/app.js
@@ -2,29 +2,57 @@ function closePopup(popupId) {
   document.getElementById(popupId).style.display = "none";
 }
 
-function initPopup() {
-  document.getElementById('popup1').style.display = "block";
+function getCookie(name) {
+  var cookies = document.cookie.split(';');
+  for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i].trim();
+    if (cookie.indexOf(name + '=') === 0) {
+      return cookie.substring(name.length + 1);
+    }
+  }
+  return null;
+}
 
-  document.getElementById('hide1dayBtn').addEventListener('click', function() {
-    var expirationDate = new Date();
-    expirationDate.setDate(expirationDate.getDate() + 1);
-    document.cookie = 'popup1=hidden; expires=' + expirationDate.toUTCString() + '; path=/;';
+function setCookie(name, value, days) {
+  var expirationDate = new Date();
+  expirationDate.setDate(expirationDate.getDate() + days);
+  document.cookie = name + '=' + value + '; expires=' + expirationDate.toUTCString() + '; path=/;';
+}
+
+function initPopup() {
+  var popup1Cookie = getCookie('popup1');
+  if (popup1Cookie !== 'hidden') {
+    document.getElementById('popup1').style.display = "block";
+  }
+
+  document.getElementById('hide1dayBtn1').addEventListener('click', function() {
+    setCookie('popup1', 'hidden', 1);
 
     closePopup('popup1');
   });
 
-  document.getElementById('closeBtn').addEventListener('click', function() {
+  document.getElementById('closeBtn1').addEventListener('click', function() {
     closePopup('popup1');
+  });
+
+  var popup2Cookie = getCookie('popup2');
+  if (popup2Cookie !== 'hidden') {
+    document.getElementById('popup2').style.display = "block";
+  }
+
+  document.getElementById('hide1dayBtn2').addEventListener('click', function() {
+    setCookie('popup2', 'hidden', 1);
+    closePopup('popup2');
   });
 
   document.getElementById('closeBtn2').addEventListener('click', function() {
     closePopup('popup2');
   });
 }
+
 window.addEventListener('load', function() {
   initPopup();
 });
-
 var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
 var rootElement = document.documentElement;
 
