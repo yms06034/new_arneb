3c17ad957b5e83538d2c91a744ed47787f6b9f6e
yms06034 <yms06034@gamil.com>
2023-06-09 10:51:18 +0900

    ch

--- static/js/app.js
+++ static/js/app.js
@@ -24,3 +24,24 @@ function initPopup() {
 window.addEventListener('load', function() {
   initPopup();
 });
+
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+var rootElement = document.documentElement;
+
+function handleScroll() {
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+  if (rootElement.scrollTop / scrollTotal > 0.15) {
+    scrollToTopBtn.classList.add("showBtn");
+  } else {
+    scrollToTopBtn.classList.remove("showBtn");
+  }
+}
+
+function scrollToTop() {
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+document.addEventListener("scroll", handleScroll);
